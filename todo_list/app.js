5d75f1ba8f31961c8721dd3d6f3b62ab0f0532f8
Dallal1 <abw090571@gmail.com>
2023-09-27 11:33:54 +0200

    finsh projekt

--- todo_list/app.js
+++ todo_list/app.js
@@ -1,2 +1,60 @@
-let name = "Mohammad";
-console.log(name);
+//Selectors
+const todoBtn = document.querySelector(".todo-btn");
+const todoInp = document.querySelector(".todo-Inp");
+const todoList = document.querySelector(".todo_list");
+
+//Event Listeners
+todoBtn.addEventListener("click", addTask);
+todoList.addEventListener("click", chakOrDellet);
+
+//Functions
+function addTask(e) {
+    e.preventDefault();
+
+    //creat li
+    const todoLi = document.createElement("li");
+    todoLi.classList.add("todo");
+
+    //creat div
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("task_name");
+
+    //add input value to div
+    todoDiv.innerText = todoInp.value;
+
+    //creat button complete
+    const completeBtn = document.createElement("button");
+    completeBtn.classList.add("todo_chech");
+    completeBtn.innerHTML = '<i class="fas fa-check"></i>';
+
+    //create button trash
+    const trashBtn = document.createElement("button");
+    trashBtn.classList.add("todo_trash");
+    trashBtn.innerHTML = '<i class="fas fa-trash"></i>';
+
+    //add div + button 1 + button 2 -> li
+    todoLi.appendChild(todoDiv);
+    todoLi.appendChild(completeBtn);
+    todoLi.appendChild(trashBtn);
+
+    //add li to ul
+    todoList.appendChild(todoLi);
+
+    //remove input value
+    todoInp.value = "";
+}
+
+function chakOrDellet(e) {
+    const item = e.target;
+
+    if (item.classList[0] === "todo_chech") {
+        const todo = item.parentElement;
+        todo.classList.toggle("completed")
+    }
+
+    if (item.classList[0] === "todo_trash") {
+        const todo = item.parentElement;
+        todo.classList.toggle("fall")
+        todo.remove();
+    }
+}
